4585dd2310530592b7fe46bda3d68c104d8c2c6d
PHLArty <long151103@gmail.com>
2024-12-05 10:11:36 +0700

    merge code arty and Loc

--- src/pages/listing/ViewListing.tsx
+++ src/pages/listing/ViewListing.tsx
@@ -108,24 +108,8 @@ const ViewListing: React.FC = () => {
         navigate(`/chat-dashboard?listingId=${listing?.id}&otherUserId=${listing?.userId}`);
     }, [listing, user]);
 
-    const handlePayment = async () => {
-        if (!user) {
-            navigate('/login'); // Redirect to login if user is null
-            return;
-        }
-
-        if (!listing) {
-            Swal.fire('Error', 'No product selected!', 'error');
-            return;
-        }
-
-        try {
-            const paymentUrl = await getPayment(listing.price, listing.id, user.id);
-            console.log(paymentUrl);
-            window.location.href = paymentUrl; // Chuyển hướng tới URL trả về từ API
-        } catch (error: any) {
-            Swal.fire('Payment Error', error?.message || 'An unknown error occurred.', 'error');
-        }
+    const handleBuyNow = () => {
+        navigate("/checkout", { state: { user, listing } });
     };
 
     if (loading) return <div>Loading...</div>;
@@ -250,56 +234,7 @@ const ViewListing: React.FC = () => {
                                                 <h5 className="font-manrope font-semibold text-2xl leading-9 text-gray-900">
                                                     <p>{Number(listing.price).toLocaleString('vi-VN')} VND</p>
                                                 </h5>
-                                                <span
-                                                    className="ml-3 font-semibold text-lg text-indigo-600">30% off</span>
                                             </div>
-                                            <svg className="mx-5 max-[400px]:hidden" xmlns="http://www.w3.org/2000/svg"
-                                                 width="2"
-                                                 height="36" viewBox="0 0 2 36" fill="none">
-                                                <path d="M1 0V36" stroke="#E5E7EB"/>
-                                            </svg>
-                                            <button
-                                                className="flex items-center gap-1 rounded-lg bg-amber-400 py-1.5 px-2.5 w-max">
-                                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none"
-                                                     xmlns="http://www.w3.org/2000/svg">
-                                                    <g clip-path="url(#clip0_12657_16865)">
-                                                        <path
-                                                            d="M8.10326 2.26718C8.47008 1.52393 9.52992 1.52394 9.89674 2.26718L11.4124 5.33818C11.558 5.63332 11.8396 5.83789 12.1653 5.88522L15.5543 6.37768C16.3746 6.49686 16.7021 7.50483 16.1086 8.08337L13.6562 10.4738C13.4205 10.7035 13.313 11.0345 13.3686 11.3589L13.9475 14.7343C14.0877 15.5512 13.2302 16.1742 12.4966 15.7885L9.46534 14.1948C9.17402 14.0417 8.82598 14.0417 8.53466 14.1948L5.5034 15.7885C4.76978 16.1742 3.91235 15.5512 4.05246 14.7343L4.63137 11.3589C4.68701 11.0345 4.57946 10.7035 4.34378 10.4738L1.89144 8.08337C1.29792 7.50483 1.62543 6.49686 2.44565 6.37768L5.8347 5.88522C6.16041 5.83789 6.44197 5.63332 6.58764 5.33818L8.10326 2.26718Z"
-                                                            fill="white"/>
-                                                        <g clip-path="url(#clip1_12657_16865)">
-                                                            <path
-                                                                d="M8.10326 2.26718C8.47008 1.52393 9.52992 1.52394 9.89674 2.26718L11.4124 5.33818C11.558 5.63332 11.8396 5.83789 12.1653 5.88522L15.5543 6.37768C16.3746 6.49686 16.7021 7.50483 16.1086 8.08337L13.6562 10.4738C13.4205 10.7035 13.313 11.0345 13.3686 11.3589L13.9475 14.7343C14.0877 15.5512 13.2302 16.1742 12.4966 15.7885L9.46534 14.1948C9.17402 14.0417 8.82598 14.0417 8.53466 14.1948L5.5034 15.7885C4.76978 16.1742 3.91235 15.5512 4.05246 14.7343L4.63137 11.3589C4.68701 11.0345 4.57946 10.7035 4.34378 10.4738L1.89144 8.08337C1.29792 7.50483 1.62543 6.49686 2.44565 6.37768L5.8347 5.88522C6.16041 5.83789 6.44197 5.63332 6.58764 5.33818L8.10326 2.26718Z"
-                                                                fill="white"/>
-                                                        </g>
-                                                    </g>
-                                                    <defs>
-                                                        <clipPath id="clip0_12657_16865">
-                                                            <rect width="18" height="18" fill="white"/>
-                                                        </clipPath>
-                                                        <clipPath id="clip1_12657_16865">
-                                                            <rect width="18" height="18" fill="white"/>
-                                                        </clipPath>
-                                                    </defs>
-                                                </svg>
-                                                <span className="text-base font-medium text-white">4.8</span>
-                                            </button>
-                                        </div>
-                                        <p className="font-medium text-lg text-gray-900 mb-2">Color</p>
-                                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mb-6 max-w-sm">
-                                            {images.map((image, index) => (
-                                                <div key={index} className="color-box group">
-                                                    <div>
-                                                        <img
-                                                            src={image}
-                                                            alt={`Thumbnail ${index + 1}`}
-                                                            className="min-[400px]:h-[100px] aspect-square border-2 border-gray-100 rounded-xl transition-all duration-500 group-hover:border-indigo-600 object-cover"
-                                                        />
-                                                        <p className="font-normal text-sm leading-6 text-gray-400 text-center mt-2 group-hover:text-indigo-600">
-                                                            Color
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            ))}
                                         </div>
                                         <div className="flex items-center flex-col min-[400px]:flex-row gap-3 mb-3 min-[400px]:mb-8">
                                             {/*  Buy listing  */}
@@ -307,7 +242,6 @@ const ViewListing: React.FC = () => {
                                                 onClick={handleBuyNow}>
                                                 Buy now
                                             </button>
-
                                         </div>
                                         {/*<button className="text-center w-full px-5 py-4 rounded-[100px] bg-indigo-600 text-white font-semibold shadow-sm shadow-transparent transition-all duration-300 hover:shadow-indigo-300">*/}
 
